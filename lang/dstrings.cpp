c6a89fb8ed905ce35a879df03d14beb52bfb160a
txkoutny <txkoutny@kiv.zcu.cz>
2017-08-04 12:13:52 +0200

    adding the strings to the common base

---
+++ lang/dstrings.cpp
@@ -0,0 +1,7 @@
+#include "dstrings.h"
+
+const char* dsFile = "File";
+const char* dsQuit = "Quit";
+
+const char* dsClose = "Close";
+const char* dsClose_All = "Close All";
---
+++ lang/dstrings.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//UI strings used by the entire application
+//all strings must be UTF-8 encoded
+
+extern const char* dsFile;
+extern const char* dsQuit;
+
+extern const char* dsClose;
+extern const char* dsClose_All;
