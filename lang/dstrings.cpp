7053b196b2a7ab5a798471bf258175ae329f64d4
txkoutny <txkoutny@kiv.zcu.cz>
2017-08-16 15:59:42 +0200

    strings added due to basic layout of the filters window

--- lang/dstrings.cpp
+++ lang/dstrings.cpp
@@ -2,6 +2,41 @@
 
 const char* dsFile = "File";
 const char* dsQuit = "Quit";
+const char* dsTools = "Tools";
+const char* dsFilters = "Filters";
 
 const char* dsClose = "Close";
 const char* dsClose_All = "Close All";
+
+const char* dsTile_Vertically = "&Tile Vertically";
+const char* dsTile_Horizontally = "&Tile Horizontally";
+const char* dsCascade = "&Cascade";
+const char* dsNext = "Ne&xt";
+const char* dsPrevious = "Pre&vious";
+
+const char* dsWindow = "Window";
+const char* dsHelp = "&Help";
+const char* dsAbout = "About";
+const char* dsAbout_Amp = "&About";
+const char* dsGlucose_Prediction = "Glucose Prediction - %1";
+
+const char* dsAdd = "Add";
+const char* dsDelete = "Delete";
+const char* dsMove_Up = "Move up";
+const char* dsMove_Down = "Move down";
+
+const char* dsFilters_Window = "Filters window";
+const char* dsApplied_Filters = "Applied filters";
+const char* dsAvailable_Filters = "Available filters";
+
+
+
+//--------------------------------- do not translate any of the rs-prefixed texts --
+
+const char* rsAbout_Text =	"<b>Glucose Predictor</b>, Version 3.0 Alpha<br>&nbsp;<br>"\
+							"Tom\xc3\xa1\xc5\xa1 Koutn\xc3\xbd, Ph.D.<br><i>txkoutny@kiv.zcu.cz</i><br>&nbsp;<br>"\
+							"Department of Computer Science and Engineering<br>"\
+							"University of West Bohemia<br>"\
+							"Plze\xc5\x88 306 14, Czech Republic<br>"\
+							"&nbsp;<hr>diabetes.zcu.cz";
+							
--- lang/dstrings.h
+++ lang/dstrings.h
@@ -6,6 +6,33 @@
 
 extern const char* dsFile;
 extern const char* dsQuit;
+extern const char* dsTools;
+extern const char* dsFilters;
 
 extern const char* dsClose;
 extern const char* dsClose_All;
+
+extern const char* dsTile_Vertically;
+extern const char* dsTile_Horizontally;
+extern const char* dsCascade;
+extern const char* dsNext;
+extern const char* dsPrevious;
+
+extern const char* dsWindow;
+extern const char* dsHelp;
+extern const char* dsAbout;
+extern const char* dsAbout_Amp;
+extern const char* dsGlucose_Prediction;
+
+extern const char* dsAdd;
+extern const char* dsDelete;
+extern const char* dsMove_Up;
+extern const char* dsMove_Down;
+
+extern const char* dsFilters_Window;
+extern const char* dsApplied_Filters;
+extern const char* dsAvailable_Filters;
+
+//--------------------------------- do not translate any of the rs-prefixed texts --
+
+extern const char* rsAbout_Text;
