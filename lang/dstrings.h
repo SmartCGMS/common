e6ce86716a8bf349d908aae3a85598a933bd12e7
txkoutny <txkoutny@kiv.zcu.cz>
2017-08-21 08:55:04 +0200

    staging

--- lang/dstrings.h
+++ lang/dstrings.h
@@ -6,6 +6,24 @@
 
 extern const char* dsFile;
 extern const char* dsQuit;
+extern const char* dsTools;
+extern const char* dsFilters;
 
 extern const char* dsClose;
 extern const char* dsClose_All;
+
+extern const char* dsTile_Vertically;
+extern const char* dsTile_Horizontally;
+extern const char* dsCascade;
+extern const char* dsNext;
+extern const char* dsPrevious;
+
+extern const char* dsWindow;
+extern const char* dsHelp;
+extern const char* dsAbout;
+extern const char* dsAbout_Amp;
+extern const char* dsGlucose_Prediction;
+
+//--------------------------------- do not translate any of the rs-prefixed texts --
+
+extern const char* rsAbout_Text;
