83ee68b39cbab9dc1841c9878cbe0fcd567ef018
txkoutny <txkoutny@kiv.zcu.cz>
2018-02-12 15:24:21 +0100

    Describe_Non_Specialized_Solver

--- rtl/descriptor_utils.h
+++ rtl/descriptor_utils.h
@@ -8,3 +8,13 @@ HRESULT do_get_descriptors(const std::vector<T> &descriptors, T **begin, T **end
 	*end = *begin + descriptors.size();
 	return S_OK;
 }
+
+constexpr glucose::TSolver_Descriptor Describe_Non_Specialized_Solver(const GUID id, const wchar_t* desc) {
+	return glucose::TSolver_Descriptor{
+		id,
+		desc,
+		false,
+		0,
+		nullptr
+	};
+}
