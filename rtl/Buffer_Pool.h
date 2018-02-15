4e4d9fc6b727c5b35ae33bd6dc49ae0241f73d3b
txkoutny <txkoutny@kiv.zcu.cz>
2018-02-15 13:50:28 +0100

    implemented base diffusion ist prediction

--- rtl/Buffer_Pool.h
+++ rtl/Buffer_Pool.h
@@ -9,40 +9,49 @@ namespace internal {
 }
 
 template <typename T>
+class CBuffer_Pool;
+
+template <typename T>
+class CPooled_Buffer {
+	//cannot inherite due to Eigen => T element
+protected:
+	CBuffer_Pool<T> &mPool;
+	T mElement;
+public:
+	CPooled_Buffer(CBuffer_Pool<T> &pool, T& element) : mPool(pool), mElement(std::move(element)) {
+	}
+
+	~CPooled_Buffer() {
+		mPool.push(std::move(mElement));
+	}
+
+	T& element() {
+		return mElement;
+	};
+};
+
+template <typename T>
 class CBuffer_Pool {
+	friend CPooled_Buffer<T>;
 protected:
 	tbb::concurrent_queue<T> mPool;
 	internal::TResize_Lambda<T> mResize_Func;
-public:
-	CBuffer_Pool(internal::TResize_Lambda<T> resize_func) : mResize_Func(resize_func) {};
-
-	T pop(const size_t minimum_size) {
-		T result;
-		mPool.try_pop(result);
-		mResize_Func(result, minimum_size);
-		return result;
-	}
-
 
 	void push(T&& elem) {
 		mPool.push(elem);
 	}
-};
 
-
-template <typename T>
-class CPooled_Buffer {
-		//cannot inherite due to Eigen => T element
-protected:
-	CBuffer_Pool<T> &mPool;
 public:
-	CPooled_Buffer(CBuffer_Pool<T> pool, const size_t minimum_size) : mPool(pool) { 
-		element = mPool.pop(minimum_size);
+	CBuffer_Pool(internal::TResize_Lambda<T> resize_func) : mResize_Func(resize_func) {};
+
+	CPooled_Buffer<T> pop(const size_t minimum_size) {
+		T buffer;
+		mPool.try_pop(buffer);
+		mResize_Func(buffer, minimum_size);
+		
+		return CPooled_Buffer<T> (*this, buffer);
 	}
 
-	~CPooled_Buffer() {
-		mPool.push(std::move(element));
-	}
-
-	T element;
 };
+
+
