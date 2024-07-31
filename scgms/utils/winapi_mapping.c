1292c5a2663cd4976ad3750ec0eaac420dc886c0
Martin Ubl <ublm@kiv.zcu.cz>
2024-07-31 10:21:54 +0200

    utils/winapi_mapping: check for return value of posix_memalign

--- scgms/utils/winapi_mapping.c
+++ scgms/utils/winapi_mapping.c
@@ -89,7 +89,13 @@ EXTERN_C void* _aligned_malloc(size_t n, size_t alignment)
 #if defined(__ARM_ARCH_7A__) || defined(__aarch64__)
 	mem = malloc(n);
 #else
-	posix_memalign(&mem, alignment, n);
+	auto res = posix_memalign(&mem, alignment, n);
+	if (res != 0) {
+		if (mem) {
+			_aligned_free(mem);
+		}
+		return nullptr;
+	}
 #endif
 
 	return mem;
